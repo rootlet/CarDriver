6455a648ad29ee090806cdf93821dbaaae110cee
PavlenkoS <rootlet@rambler.ru>
2017-11-15 01:39:14 +0300

    Исправлены ошибки шины

--- core/src/com/rootlet/cardriver/world/Car.java
+++ core/src/com/rootlet/cardriver/world/Car.java
@@ -38,33 +38,28 @@ public class Car {
 
     private Vector2 getLeteralVelocity() {
         Vector2 currentRightNormal = tire.getWorldVector(new Vector2(1,0));
-        return currentRightNormal.scl(currentRightNormal.dot(tire.getLinearVelocity()));
+        return new Vector2(currentRightNormal).scl(currentRightNormal.dot(tire.getLinearVelocity()));
     }
 
     private Vector2 getForwardVelocity() {
         Vector2 currentForwardNormal = tire.getWorldVector(new Vector2(0,1));
-        System.out.println("currentForwardNormal = " + currentForwardNormal);
-        System.out.println("tire.getLinearVelocity() = " + tire.getLinearVelocity());
-        System.out.println("currentForwardNormal.dot(tire.getLinearVelocity()) = " + currentForwardNormal.dot(tire.getLinearVelocity()));
-        return currentForwardNormal.scl(currentForwardNormal.dot(tire.getLinearVelocity()));
+        return new Vector2(currentForwardNormal).scl(currentForwardNormal.dot(tire.getLinearVelocity()));
     }
 
     public void updateFriction() {
-        System.out.println(getForwardVelocity());
-        Vector2 impulse = getLeteralVelocity().scl(-tire.getMass());
+        Vector2 impulse = new Vector2(getLeteralVelocity()).scl(-tire.getMass());
         //-------------------Занос---------------------------------------------
-        //if ( impulse.len() > 2.5f )
-        //    impulse = impulse.scl(2.5f / impulse.len());
+        if ( impulse.len() > 2.5f )
+            impulse = new Vector2(impulse).scl(2.5f / impulse.len());
         //----------------------------------------------------------------------
-        tire.applyLinearImpulse(impulse, tire.getWorldCenter(), false);
+        tire.applyLinearImpulse(impulse, tire.getWorldCenter(), true);
 
-        tire.applyAngularImpulse(0.1f * tire.getInertia() * -tire.getAngularVelocity(), false);
+        tire.applyAngularImpulse(0.1f * tire.getInertia() * -tire.getAngularVelocity(), true);
 
         Vector2 currentForwardNormal = getForwardVelocity();
         float currentForwardSpeed = normalize(currentForwardNormal);
-        //value = currentForwardNormal.len();
         float dragForceMagnitude = -2 * currentForwardSpeed;
-        tire.applyForce( currentForwardNormal.scl(dragForceMagnitude ), tire.getWorldCenter(), false );
+        tire.applyForce( new Vector2(currentForwardNormal).scl(dragForceMagnitude ), tire.getWorldCenter(), true );
     }
 
     /// Convert this vector into a unit vector. Returns the length.
@@ -83,9 +78,9 @@ public class Car {
     }
 
     //tire class variables
-    float maxForwardSpeed = 100;  // 100;
+    float maxForwardSpeed = 10;  // 100;
     float maxBackwardSpeed = -2; // -20;
-    float maxDriveForce = 150;    // 150;
+    float maxDriveForce = 15;    // 150;
 
     public enum Control{
         NONE,
@@ -116,12 +111,7 @@ public class Car {
             force = -maxDriveForce;
         else
             return;
-
-       // if (currentSpeed == 0) {
-            tire.applyForce(new Vector2(0, force), tire.getWorldCenter(), true);
-        //} else {
-           // tire.applyForce(currentForwardNormal.scl(force),tire.getWorldCenter(), true);
-        //}
+        tire.applyForce(new Vector2(currentForwardNormal).scl(force),tire.getWorldCenter(), true);
     }
 
     public void updateTurn(Control controlState) {
@@ -131,7 +121,7 @@ public class Car {
             case RIGHT: desiredTorque = -15; break;
             default: break;//nothing
         }
-        tire.applyTorque( desiredTorque, false );
+        tire.applyTorque( desiredTorque, true );
     }
 
 
